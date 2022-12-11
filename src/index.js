a396b62193fccd93458f3bf0b143418f1303d0be
Foster <72973015+Foster0123@users.noreply.github.com>
2022-12-11 11:12:27 +0530

    Hero 2nd Commit

--- src/index.js
+++ src/index.js
@@ -12,10 +12,19 @@ const __dirname = path.dirname(__filename);
 
 const htmlFilePath = path.join(__dirname, "html", "index.html");
 
-const requestListener = (req, res) => {
-
-	const server = http.createServer(requestListener);
-	server.listen(port, host, () => {
-		console.log(`Server Running On ${host}:${port}`);
-	});
+const requestListener = (req ,res) => {
+	if(req.url == "/"){
+		fs.readFile(htmlFilePath, (err , data) => {
+			res.writeHead(200, {"Content-Type": "text/html"})
+			res.write(data);
+			res.end();
+		})
+	}
 }
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+	console.log(`Server Running On ${host}:${port}`);
+	console.log(`filename:${__filename}`);
+	console.log(`dirname:${__dirname}`);
+});
